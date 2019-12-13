dad86a9010d58fa5e8e34f7de3a4e255f0246a2d
cardistryj <547207686@qq.com>
2019-12-13 22:47:19 +0800

    add physics

--- F4_ns3.cc
+++ F4_ns3.cc
@@ -9,44 +9,120 @@ using namespace ns3;
 int main(int argc, char* argv[]){
 
     CommandLine cmd;
+
+    uint32_t nCsma = 3;
+    uint32_t nWifi = 3;
+    bool tracing = false;
+
+    CommandLine cmd;
+    cmd.AddValue ("nCsma", "Number of \"extra\" CSMA p2pNodes/devices", nCsma);
+    cmd.AddValue ("nWifi", "Number of wifi STA devices", nWifi);
+    cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
+    
     cmd.Parse (argc, argv);
+
+    if (nWifi > 18)
+    {
+      std::cout << "nWifi should be 18 or less; otherwise grid layout exceeds the bounding box" << std::endl;
+      return 1;
+    }
     
     Time::SetResolution (Time::NS);
     LogComponentEnable("F4ComputerNetwork",LOG_LEVEL_INFO);
     // Physical & link
-    NodeContainer nodes;
-    nodes.Create (2);
+    NodeContainer p2pNodes;
+    p2pNodes.Create (2);
 
     PointToPointHelper pointToPoint;
     pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
     pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
-    NetDeviceContainer devices;
-    devices = pointToPoint.Install (nodes);
+    NetDeviceContainer p2pDevices;
+    p2pDevices = pointToPoint.Install (p2pNodes);
+
+    NodeContainer csmaNodes;
+    csmaNodes.Add (p2pNodes.Get (1));
+    csmaNodes.Create (nCsma);
+
+    CsmaHelper csma;
+    csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
+    csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
     
+    NetDeviceContainer csmaDevices;
+    csmaDevices = csma.Install (csmaNodes);
+
+    NodeContainer wifiStaNodes;
+    wifiStaNodes.Create (nWifi);
+    NodeContainer wifiApNode = p2pNodes.Get (0);
+
+    YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
+    YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
+    phy.SetChannel (channel.Create ());
+
+    WifiHelper wifi;
+    wifi.SetRemoteStationManager ("ns3::AarfWifiManager");
+
+    WifiMacHelper mac;
+    Ssid ssid = Ssid ("ns-3-ssid");
+    mac.SetType ("ns3::StaWifiMac",
+                "Ssid", SsidValue (ssid),
+                "ActiveProbing", BooleanValue (false));
+
+    NetDeviceContainer staDevices;
+    staDevices = wifi.Install (phy, mac, wifiStaNodes);
+
+    mac.SetType ("ns3::ApWifiMac",
+                "Ssid", SsidValue (ssid));
+
+    NetDeviceContainer apDevices;
+    apDevices = wifi.Install (phy, mac, wifiApNode);
+
+    MobilityHelper mobility;
+
+    mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
+                                    "MinX", DoubleValue (0.0),
+                                    "MinY", DoubleValue (0.0),
+                                    "DeltaX", DoubleValue (5.0),
+                                    "DeltaY", DoubleValue (10.0),
+                                    "GridWidth", UintegerValue (3),
+                                    "LayoutType", StringValue ("RowFirst"));
+
+    mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
+                                "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));
+    mobility.Install (wifiStaNodes);
+
+    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+    mobility.Install (wifiApNode);
 
     // inter-networking
     InternetStackHelper stack;
-    stack.Install (nodes);
+    stack.Install (csmaNodes);
+    stack.Install (wifiApNode);
+    stack.Install (wifiStaNodes);
 
     NS_LOG_INFO("hello world");
     Ipv4AddressHelper address;
     address.SetBase ("10.1.1.0", "255.255.255.0");
 
-    Ipv4InterfaceContainer interfaces = address.Assign (devices);
+    address.SetBase ("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer p2pInterfaces;
+    p2pInterfaces = address.Assign (p2pDevices);
 
-    // transport
+    address.SetBase ("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer csmaInterfaces;
+    csmaInterfaces = address.Assign (csmaDevices);
 
-
-
-
+    address.SetBase ("10.1.3.0", "255.255.255.0");
+    address.Assign (staDevices);
+    address.Assign (apDevices);
 
     // 应用层
     // 需要的输入是一个已经安装了 TCP 协议的 server 以及它的 ns3::Ipv4Address 
     // 一个数组，包含一组已经安装了 TCP 协议的 client
     std::vector<Ptr<Node>> clients;
-    clients.push_back(nodes.Get(1));
-    vStompApplication application = vStompApplication(nodes.Get(0),interfaces.GetAddress(0),clients);
+    clients.push_back(wifiStaNodes.Get(nWifi-1));
+    clients.push_back(wifiStaNodes.Get(1));
+    vStompApplication application = vStompApplication(csmaNodes.Get(nCsma),csmaInterfaces.GetAddress(nCsma),clients);
     application.start();
 
     Simulator::Stop(Seconds(20));
