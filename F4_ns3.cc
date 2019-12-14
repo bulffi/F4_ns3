c7af5067e277819cc537b7170023abb0c4deaaf9
Zijian Zhang <39160610+bulffi@users.noreply.github.com>
2019-12-14 14:36:40 +0800

    fix logging

--- F4_ns3.cc
+++ F4_ns3.cc
@@ -24,6 +24,8 @@ int main(int argc, char* argv[]){
 
     cmd.Parse (argc, argv);
 
+    LogComponentEnable("F4ComputerNetwork",LOG_LEVEL_INFO);
+
     ConfigStore inputConfig;
     inputConfig.ConfigureDefaults ();
 
@@ -52,7 +54,7 @@ int main(int argc, char* argv[]){
     ipv4h.SetBase ("1.0.0.0", "255.0.0.0");
     Ipv4InterfaceContainer internetIpIfaces = ipv4h.Assign (internetDevices);
     // interface 0 is localhost, 1 is the p2p device
-    Ipv4Address remoteHostAddr = internetIpIfaces.GetAddress (1);
+    //Ipv4Address remoteHostAddr = internetIpIfaces.GetAddress (1);
 
     Ipv4StaticRoutingHelper ipv4RoutingHelper;
     Ptr<Ipv4StaticRouting> remoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting (remoteHost->GetObject<Ipv4> ());
