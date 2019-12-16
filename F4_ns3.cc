4a7efa7af7f9b333d4c6fe7b8aeb246858583e48
cardistryj <547207686@qq.com>
2019-12-16 19:43:20 +0800

    fix bugs

--- F4_ns3.cc
+++ F4_ns3.cc
@@ -4,17 +4,16 @@
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/mobility-module.h"
-#include "ns3/csma-module.h"
 #include "ns3/lte-module.h"
 #include "ns3/config-store-module.h"
-#include <ns3/buildings-helper.h>
 #include "vstomp.h"
 using namespace ns3;
 
 int main(int argc, char* argv[]){
 
-    uint16_t numEnb = 2;
+    uint16_t numEnb = 3;
     uint16_t numUePerEnb = 3;
+    double distance = 60.0;
     bool tracing = true;
 
     CommandLine cmd;
@@ -65,13 +64,25 @@ int main(int argc, char* argv[]){
     enbNodes.Create (numEnb);
     ueNodes.Create (numEnb*numUePerEnb);
 
+    Ptr<ListPositionAllocator> positionAllocEnb = CreateObject<ListPositionAllocator> ();
+    for (uint16_t i = 0; i < numEnb; i++)
+    {
+      positionAllocEnb->Add (Vector (distance * i, 0, 0));
+    }
+    Ptr<ListPositionAllocator> positionAllocEu = CreateObject<ListPositionAllocator> ();
+    for (uint16_t i = 0; i < numEnb; i++)
+    {
+      for(uint16_t j=0;j<numUePerEnb;j++){
+        positionAllocEu->Add (Vector (distance * i + j, 0, 0));
+      }
+    }
     MobilityHelper mobility;
-    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-    mobility.Install (enbNodes);
-    BuildingsHelper::Install (enbNodes);
-    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-    mobility.Install (ueNodes);
-    BuildingsHelper::Install (ueNodes);
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobility.SetPositionAllocator(positionAllocEnb);
+    mobility.Install(enbNodes);
+    mobility.SetPositionAllocator(positionAllocEu);
+    mobility.Install(ueNodes);
+
 
     // Install LTE Devices to the nodes
     NetDeviceContainer enbLteDevs = lteHelper->InstallEnbDevice (enbNodes);
@@ -82,7 +93,7 @@ int main(int argc, char* argv[]){
     Ipv4InterfaceContainer ueIpIface;
     ueIpIface = epcHelper->AssignUeIpv4Address (NetDeviceContainer (ueLteDevs));
     // Assign IP address to UEs, and install applications
-    for (uint32_t u = 0; u < ueNodes.GetN (); ++u)
+    for (uint16_t u = 0; u < ueNodes.GetN (); ++u)
       {
         Ptr<Node> ueNode = ueNodes.Get (u);
         // Set the default gateway for the UE
@@ -103,95 +114,31 @@ int main(int argc, char* argv[]){
     // 需要的输入是一个已经安装了 TCP 协议的 server 以及它的 ns3::Ipv4Address 
     // 一个数组，包含一组已经安装了 TCP 协议的 client
 
-
-    //client1
     ClientWithMessages client1;
-    // client1.node = pgw;
-    client1.node = ueNodes.Get(0);
+    client1.node = ueNodes.Get(1);
     std::vector<std::string> channels;
-    channels.push_back("LifeInTongji");
-    channels.push_back("ComputerNetWork");
+    channels.push_back("hhh");
+    channels.push_back("xixi");
     std::vector<Message> messages;
     Message message1;
     message1.target="zzj";
     message1.content="say hello to myself";
     Message message2;
-    message2.target="ComputerNetWork";
+    message2.target="hhh";
     message2.content="try out my channel";
     messages.push_back(message1);
     messages.push_back(message2);
     client1.userName="zzj";
     client1.channelsTosub = channels;
     client1.messagesToSend = messages;
-
-    //client2
-    ClientWithMessages client2;
-    client2.userName = "lh";
-    std::vector<std::string> channel2;
-    channel2.push_back("ComputerNetWork");
-    std::vector<Message> messages2;
-    Message message2_1;
-    message2_1.target = "zzj";
-    message2_1.content = "Hello! My name is lh, nice to meet you!";
-    Message message2_2;
-    message2_2.target="ComputerNetWork";
-    message2_2.content="I have a question! Why is TCP/IP important?";
-    messages2.push_back(message2_1);
-    messages2.push_back(message2_2);
-    client2.channelsTosub = channel2;
-    client2.messagesToSend = messages2;
-    // 主要是这里应该选择哪个点？
-    client2.node = ueNodes.Get(1);
-
-    //client 3
-    ClientWithMessages client3;
-    client3.userName = "jxh";
-    std::vector<std::string> channel3;
-    channel3.push_back("LifeInTongji");
-    std::vector<Message> messages3;
-    Message message3_1;
-    message3_1.target = "lh";
-    message3_1.content = "You are my roommate!";
-    Message message3_2;
-    message3_2.target="ComputerNetWork";
-    message3_2.content="I don't need to subscribe to this channel to send you a message!";
-    messages3.push_back(message3_1);
-    messages3.push_back(message3_2);
-    client3.channelsTosub = channel3;
-    client3.messagesToSend = messages3;
-    // 主要是这里应该选择哪个点？
-    client3.node = ueNodes.Get(2);
-
-    //client 4
-    ClientWithMessages client4;
-    client4.userName = "lym";
-    std::vector<std::string> channel4;
-    channel4.push_back("hhh");
-    std::vector<Message> messages4;
-    Message message4_1;
-    message4_1.target = "jxh";
-    message4_1.content = "Have you take your breakfast?";
-    Message message4_2;
-    message4_2.target="ComputerNetWork";
-    message4_2.content="Have you finished your homework?";
-    messages4.push_back(message4_1);
-    messages4.push_back(message4_2);
-    client4.channelsTosub = channel4;
-    client4.messagesToSend = messages4;
-    // 主要是这里应该选择哪个点？
-    client4.node = ueNodes.Get(3);
-
     // ClientWithMessages client2;
     // ClientWithMessages client3;
     std::vector<ClientWithMessages> clients;
     clients.push_back(client1);
-    clients.push_back(client2);
-    clients.push_back(client3);
-    clients.push_back(client4);
     vStompApplication application = vStompApplication(remoteHost,remoteHostAddr,clients);
     application.start();
 
-    Simulator::Stop(Seconds(50));
+    Simulator::Stop(Seconds(20));
     NS_LOG_INFO("The simulation begins");
 
     if (tracing == true)
